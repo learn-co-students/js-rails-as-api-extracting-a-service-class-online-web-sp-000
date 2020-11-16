df282f9fe4420b7edb01d65ab65b2c7e2f2bb676
johncban <johncban@gmail.com>
2020-11-16 02:40:43 -0500

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,51 @@
 class SightingsController < ApplicationController
+    def index
+        sightings = Sighting.all
+        '''
+        render json: sightings.to_json(:include => {
+          :bird => {:only => [:name, :species]},
+          :location => {:only => [:latitude, :longitude]}
+        }, :except => [:updated_at])
+        '''
+        render json: SightingSerializer.new(sightings).to_serialized_json
+
+    end
+    
     def show
-        @sighting = Sighting.find(params[:id])
-        render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+        # @sighting = Sighting.find(params[:id])
+        # render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+        sighting = Sighting.find_by(id: params[:id])
+        '''
+        render json: sighting.to_json(:include => {
+          :bird => {:only => [:name, :species]},
+          :location => {:only => [:latitude, :longitude]}
+        }, :except => [:updated_at])
+        '''
+        render json: SightingSerializer.new(sighting).to_serialized_json
+    end
+    
+    def initialize(sighting_object)
+        @sighting = sighting_object
+    end
+    
+    def to_serialized_json
+        '''
+        @sighting.to_json(:include => {
+          :bird => {:only => [:name, :species]},
+          :location => {:only => [:latitude, :longitude]}
+        }, :except => [:updated_at])
+        '''
+        options = {
+            include: {
+              bird: {
+                only: [:name, :species]
+              },
+              location: {
+                only: [:latitude, :longitude]
+              }
+            },
+            except: [:updated_at],
+        }
+        @sighting.to_json(options)
     end
 end
