3470f73a958d9f0d502d8afa3db9d746d1db5541
anpham148 <anpham148@gmail.com>
2021-02-06 12:26:29 +0000

    Done.

---
+++ app/services/sighting_serializer.rb
@@ -0,0 +1,22 @@
+class SightingSerializer
+
+    def initialize(sighting_obj)
+        @sighting = sighting_obj
+    end
+
+    def to_serialized_json
+        options = {
+          include: {
+            bird: {
+                only: [:name, :species]
+            },
+            location: {
+                only: [:latitude, :longitude]
+            }
+          }, 
+          except: [:updated_at]
+        }
+            @sighting.to_json(options)
+        
+    end
+end
