0ce26b033b192a7cc968dd5729c4c57941175fa1
rmholsen <jaguar.kitty@gmail.com>
2021-06-01 13:07:31 -0400

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,23 @@
 class SightingsController < ApplicationController
-    def show
-        @sighting = Sighting.find(params[:id])
-        render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
-    end
+    def index
+        sightings = Sighting.all
+        # NEW HOTNESS
+        render json: SightingSerializer.new(sightings).to_serialized_json
+        # OLD AND BUSTED
+        # render json: sightings.to_json(:include => {
+        #   :bird => {:only => [:name, :species]},
+        #   :location => {:only => [:latitude, :longitude]}
+        # }, :except => [:updated_at])
+      end
+     
+      def show
+        sighting = Sighting.find_by(id: params[:id])
+        # NEW HOTNESS
+        render json: SightingSerializer.new(sighting).to_serialized_json
+        # OLD AND BUSTED
+        # render json: sighting.to_json(:include => {
+        #   :bird => {:only => [:name, :species]},
+        #   :location => {:only => [:latitude, :longitude]}
+        # }, :except => [:updated_at])
+      end
 end
